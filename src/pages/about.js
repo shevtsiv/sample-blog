a4fcf94ebb850bb26c0e313fba9560a757588166
shevtsiv <rostykshevtsiv@gmail.com>
2020-04-20 00:13:31 +0300

    Implement 'About' and 'Donate' pages content acquisition

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,28 @@ import * as React from "react";
 import NavMenu from "../header/menu";
 import Container from "react-bootstrap/Container";
 import '../index.css';
+import marked from 'marked';
 
 export default class AboutPage extends React.Component {
+
+  state = {
+    markedContent: ""
+  };
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_BACKEND_URL + '/about')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({markedContent: marked(data.content)})
+      })
+      .catch(console.error)
+  }
 
   render() {
     return (
       <NavMenu>
-        <Container className="container" style={{maxWidth: 900}}>
-          {/*  TODO: Implement About page */}
+        <Container style={{marginTop: '80px'}}>
+          <div dangerouslySetInnerHTML={{__html: this.state.markedContent}}/>
         </Container>
       </NavMenu>
     );
