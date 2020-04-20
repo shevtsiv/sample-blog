d15c7a1785efd730819ceb66aa9a93a723302ede
shevtsiv <rostykshevtsiv@gmail.com>
2020-04-20 12:26:35 +0300

    Make Navbar links more responsive

--- src/header/menu.js
+++ src/header/menu.js
@@ -8,14 +8,12 @@ function NavMenu(props) {
     <div>
       <Navbar className="border-bottom" bg="light" expand="lg" sticky="top">
         <Navbar.Brand href="/">SampleBlog</Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Form inline>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/donate">Donate</Nav.Link>
+              <Nav.Link style={{padding: '5px 5px 5px 5px'}} href="/about">About</Nav.Link>
+              <Nav.Link style={{padding: '5px 5px 5px 5px'}} href="/donate">Donate</Nav.Link>
             </Form>
           </Nav>
-        </Navbar.Collapse>
       </Navbar>
       {props.children}
     </div>
