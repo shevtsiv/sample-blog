df10c1359fe1a5985cce8ab3871a4d47f00c1451
shevtsiv <rostykshevtsiv@gmail.com>
2020-04-20 12:08:44 +0300

    Use Navbar's built-in 'sticky' property to make navbar sticky

--- src/header/menu.js
+++ src/header/menu.js
@@ -6,8 +6,7 @@ import Form from "react-bootstrap/Form";
 function NavMenu(props) {
   return (
     <div>
-      <Navbar className="border-bottom" bg="light" expand="lg"
-              style={{zIndex: 999, width: '100%', position: 'fixed', top: 0}}>
+      <Navbar className="border-bottom" bg="light" expand="lg" sticky="top">
         <Navbar.Brand href="/">SampleBlog</Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
